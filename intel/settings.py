8c8ccd48c5fbb17cceb0bfb98b1422cc6536438c
mikegyamfi <codeage20@gmail.com>
2023-06-10 20:06:44 +0000

    initial commit

--- intel/settings.py
+++ intel/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-a%%!+)mu^q5eqi)-qe2jg@$_=(3*g5x2g4g-ep_xr*+3hzh8df
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'www.cledagh.com'
+    'www.cledagh.com',
+    'cleda-if7tf.ondigitalocean.app'
 ]
 
 
@@ -93,26 +94,26 @@ WSGI_APPLICATION = 'intel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'HOST': config("DATABASE_HOST"),
-#         'PORT': config("DATABASE_PORT"),
-#         'NAME': 'db',
-#         'USER': config("DATABASE_USERNAME"),
-#         'PASSWORD': config("DATABASE_PASSWORD"),
-#         'OPTIONS': {
-#             'sslmode': 'require'
-#         }
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': config("DATABASE_HOST"),
+        'PORT': config("DATABASE_PORT"),
+        'NAME': 'db',
+        'USER': config("DATABASE_USERNAME"),
+        'PASSWORD': config("DATABASE_PASSWORD"),
+        'OPTIONS': {
+            'sslmode': 'require'
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
